6495e356fa0c940caeaec842a234e68f7bb77359
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2020-12-16 13:58:35 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+ superbowlWin= (record) => {
+  let win=record.find(record => record.result==="W");
+  return win? win.year : undefined
+}
